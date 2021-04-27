35f6fe37a344d270c5fb15e58cf50240c2093103
PCOME <pierric.come@ynov.com>
2021-04-27 11:48:41 +0200

    ils sont mal mais pas au bon endroit

--- static/js/artists.js
+++ static/js/artists.js
@@ -1,12 +1,41 @@
-const fetchSearchArtists = "/api/artists"
-let artistsDAta;
+//const fetchSearchArtists = "/api/artists"
 
-const loadArtistsData =  (reponse) => {
-    artistsDAta = [...reponse]
-    console.log('res : ')
-    console.log(artistsDAta)
+function getArtist() {
+    let artistsData = [...searchArtistsData];
+    let band;
+    let nameBand;
+    let nameArtists;
+    let creationDate;
+    let firstDate;
+    for (let i =0; i < artistsData.length;i++) {
+        band = document.createElement('div')
+        band.classList.add('band')
+        nameBand = document.createElement('h2')
+        nameBand.innerText = artistsData[i]['name']
+        nameArtists = document.createElement('p')
+        nameArtists.innerText = artistsData[i]['name']
+        creationDate = document.createElement('p')
+        creationDate.innerText =artistsData[i]['creationDate']
+        firstDate = document.createElement('p')
+        firstDate.innerText = artistsData[i]['firstAlbum']
+        band.appendChild(nameBand)
+        band.appendChild(nameArtists)
+        band.appendChild(creationDate)
+        band.appendChild(firstDate)
+        document.querySelector('main').appendChild(band)
+    }
+     
 }
+
+/*
+
+/*const loadArtistsData =  (reponse) => {
+    artistsData = [...reponse]
+    console.log('res : ')
+    console.log(artistsData)
+}
+
 
 fetch(fetchSearchArtists)
 .then(response => response.json())
-.then(loadArtistsData)
+.then(loadArtistsData)*/

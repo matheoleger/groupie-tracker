5a313ca4f2a2f22b67eaa844666e5e964903fda7
PCOME <pierric.come@ynov.com>
2021-05-04 11:21:54 +0200

    cleaning

--- static/js/filtre.js
+++
@@ -1,8 +0,0 @@
-const filtre = (artistID) => {
-    // const artist = document.querySelector(artistID)
-    console.log(artistID)
-
-    fetch(`/artists/howMany`)
-    .then(response => response.json())
-    .then(displayFiltre)
-}
